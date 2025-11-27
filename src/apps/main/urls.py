b46fcf073ba7dab46e29cba7c19623681550f472
LEKS <ilhanchanel20002@gmail.com>
2025-11-27 08:27:36 +0300

    feature: URL and template structure

--- src/apps/main/urls.py
+++ src/apps/main/urls.py
@@ -7,5 +7,9 @@ app_name = 'main'
 
 
 urlpatterns = (
-    path("", views.HomeView.as_view()),
+    path('', views.HomeView.as_view(), name='home'),
+    path('about-us/', views.AboutUsView.as_view(), name='about-us'),
+    path('contact-us/', views.ContactUsView.as_view(), name='contact-us'),
+    path('excursions/', views.ExcursionListView.as_view(), name='excursion-list'),  # noqa
+    path('excursions/detail/', views.ExcursionDetailView.as_view(), name='excursion-detail'),  # noqa
 )
--- src/apps/main/views.py
+++ src/apps/main/views.py
@@ -3,3 +3,19 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = 'main/home.django-html'
+
+
+class AboutUsView(TemplateView):
+    template_name = 'main/about-us.django-html'
+
+
+class ContactUsView(TemplateView):
+    template_name = 'main/contact-us.django-html'
+
+
+class ExcursionListView(TemplateView):
+    template_name = 'main/excursion-list.django-html'
+
+
+class ExcursionDetailView(TemplateView):
+    template_name = 'main/excursion-detail.django-html'
