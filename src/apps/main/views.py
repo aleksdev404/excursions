dca0bba4b222e1637e5989c6d2ede42102711dec
LEKS <ilhanchanel20002@gmail.com>
2025-11-27 11:17:30 +0300

    feature: finished home page template structure

--- src/apps/main/views.py
+++ src/apps/main/views.py
@@ -2,7 +2,7 @@ from django.views.generic import TemplateView
 
 
 class HomeView(TemplateView):
-    template_name = 'main/home.django-html'
+    template_name = 'main/home/index.django-html'
 
 
 class AboutUsView(TemplateView):
